2e55d3dd3e57327fd705a575c62040033e88a484
Siddique Hameed <siddii+git@gmail.com>
2013-06-17 12:32:54 -0500

    WIP

--- js/extension.js
+++ js/extension.js
@@ -1,3 +1,18 @@
+
+function WikipediaExtensionController($scope, $http) {
+    $http.get('settings.json').success(function (settings) {
+        console.log('settings = ', settings);
+        $scope.settings = settings;
+
+        if ($scope.settings.featuredFeed) {
+            $http.get($scope.settings.featuredFeed).success(function (featuredFeeds) {
+                console.log('Featured Feeds = ', featuredFeeds);
+            });
+        }
+    })
+}
+WikipediaExtensionController.$inject = ['$scope', '$http'];
+
 angular.module('wikipedia', [], function (){
     console.log('Wikipedia module initialised');
 });
