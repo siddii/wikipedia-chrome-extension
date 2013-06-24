71d435cf9b430c74018a20e21675292520e37423
Siddique Hameed <siddii+git@gmail.com>
2013-06-24 12:25:15 -0500

    WIP

--- js/extension.js
+++ js/extension.js
@@ -1,60 +1,54 @@
 
-function WikipediaExtensionController($scope, $http, Feed) {
-//    $http.get('settings.json').success(function (settings) {
-//        console.log('settings = ', settings);
-//        $scope.settings = settings;
-//
-//        if ($scope.settings.featuredFeed) {
-//            $http.get($scope.settings.featuredFeed).success(function (featuredFeeds) {
-//                console.log('Featured Feeds = ', featuredFeeds.feed);
-//            });
-//        }
-//    });
+function GoogleAjaxFeedService($http) {
+    this.parseFeed = function(url){
+        return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    }
+}
+GoogleAjaxFeedService.$inject = ['$http'];
 
-    var extensionURL = chrome.extension.getURL('/');
-
-    $scope.loadFeed=function(){
-        Feed.parseFeed($scope.feedSrc).then(function(res){
-            $scope.feeds=res.data.responseData.feed.entries;
-            if ($scope.feeds.length > 0) {
-                $scope.feeds.sort(function (feed1, feed2) {
-                  return new Date(feed1.publishedDate) > new Date(feed2.publishedDate);
+function WikipediaFeeds(GoogleAjaxFeedService, extensionURL) {
+    this.loadFeeds = function (feedURL, baseURL) {
+        return GoogleAjaxFeedService.parseFeed(feedURL).then(function(res){
+            var feedEntries=res.data.responseData.feed.entries;
+            if (feedEntries.length > 0) {
+                feedEntries.sort(function (feed1, feed2) {
+                    return new Date(feed1.publishedDate) > new Date(feed2.publishedDate);
                 });
             }
-            for(var i=0; i < $scope.feeds.length; i++) {
-                var feedElement = angular.element('<div>' + $scope.feeds[i].content + '</div>');
+            for(var i=0; i < feedEntries.length; i++) {
+                var feedElement = angular.element('<div>' + feedEntries[i].content + '</div>');
                 feedElement.find('img').each(function (idx, imgNode) {
                     imgNode.src = imgNode.src.replace('chrome-extension://', 'http://');
                 });
                 feedElement.find('a').each(function (idx, aNode) {
                     aNode.target = "_new";
                     aNode.href = aNode.href.replace('file:', 'http:');
-                    aNode.href = aNode.href.replace(extensionURL, 'http://en.wikipedia.org/');
+                    aNode.href = aNode.href.replace(extensionURL, baseURL);
                 });
-                $scope.feeds[i].content = feedElement.html();
-                console.log('######### $scope.feeds[i].content = ', $scope.feeds[i].content);
+                feedEntries[i].content = feedElement.html();
             }
+            return feedEntries;
         });
     };
-
-    $scope.feedSrc = 'http://en.wikipedia.org/w/api.php?action=featuredfeed&feed=potd&feedformat=atom';
-    $scope.loadFeed();
-
-    $('#feed-carousel').bind('slide', function (){
-
-    });
 }
-WikipediaExtensionController.$inject = ['$scope', '$http', 'FeedService'];
+WikipediaFeeds.$inject = ['GoogleAjaxFeedService', 'extensionURL'];
+
+
+function WikipediaExtensionController($scope, WikipediaFeeds) {
+    $scope.baseURL = 'http://en.wikipedia.org/';
+    $scope.feedSrc = 'http://en.wikipedia.org/w/api.php?action=featuredfeed&feed=potd&feedformat=atom';
+    console.log('$scope.baseURL= ', $scope.baseURL, '$scope.feedSrc = ', $scope.feedSrc);
+    $scope.feeds = WikipediaFeeds.loadFeeds($scope.feedSrc, $scope.baseURL);
+}
+
+WikipediaExtensionController.$inject = ['$scope', 'WikipediaFeeds'];
 
 var App = angular.module('wikipedia', [], function (){
     console.log('Wikipedia module initialised');
 });
 
+App.service('WikipediaFeeds', WikipediaFeeds);
+App.service('GoogleAjaxFeedService', GoogleAjaxFeedService);
 
-App.factory('FeedService',['$http',function($http){
-    return {
-        parseFeed : function(url){
-            return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
-        }
-    }
-}]);
+App.value('extensionURL', chrome.extension.getURL('/'));
+
