e1c8c0a9628734650af89736c4631380d4458eac
Siddique Hameed <siddii+git@gmail.com>
2013-06-26 13:23:56 -0500

    minor refactoring

--- js/extension.js
+++ js/extension.js
@@ -1,33 +1,37 @@
 
 function GoogleAjaxFeedService($http) {
-    this.parseFeed = function(url){
-        return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    this.getFeed = function(url){
+        return $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url), {cache: true});
     }
 }
 GoogleAjaxFeedService.$inject = ['$http'];
 
 function WikipediaFeeds(GoogleAjaxFeedService, extensionURL) {
+    function parseFeeds(res, baseURL) {
+        var feedEntries = res.data.responseData.feed.entries;
+        if (feedEntries.length > 0) {
+            feedEntries.sort(function (feed1, feed2) {
+                return new Date(feed1.publishedDate) > new Date(feed2.publishedDate);
+            });
+        }
+        for (var i = 0; i < feedEntries.length; i++) {
+            var feedElement = angular.element('<div>' + feedEntries[i].content + '</div>');
+            feedElement.find('img').each(function (idx, imgNode) {
+                imgNode.src = imgNode.src.replace('chrome-extension://', 'http://');
+            });
+            feedElement.find('a').each(function (idx, aNode) {
+                aNode.target = "_new";
+                aNode.href = aNode.href.replace('file:', 'http:');
+                aNode.href = aNode.href.replace(extensionURL, baseURL);
+            });
+            feedEntries[i].content = feedElement.html();
+        }
+        return feedEntries;
+    }
+
     this.loadFeeds = function (feedURL, baseURL) {
-        return GoogleAjaxFeedService.parseFeed(feedURL).then(function(res){
-            var feedEntries=res.data.responseData.feed.entries;
-            if (feedEntries.length > 0) {
-                feedEntries.sort(function (feed1, feed2) {
-                    return new Date(feed1.publishedDate) > new Date(feed2.publishedDate);
-                });
-            }
-            for(var i=0; i < feedEntries.length; i++) {
-                var feedElement = angular.element('<div>' + feedEntries[i].content + '</div>');
-                feedElement.find('img').each(function (idx, imgNode) {
-                    imgNode.src = imgNode.src.replace('chrome-extension://', 'http://');
-                });
-                feedElement.find('a').each(function (idx, aNode) {
-                    aNode.target = "_new";
-                    aNode.href = aNode.href.replace('file:', 'http:');
-                    aNode.href = aNode.href.replace(extensionURL, baseURL);
-                });
-                feedEntries[i].content = feedElement.html();
-            }
-            return feedEntries;
+        return GoogleAjaxFeedService.getFeed(feedURL).then(function(res){
+            return parseFeeds(res, baseURL);
         });
     };
 }
