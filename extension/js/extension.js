c5cc28cc53be909d77a20e2433b9a305009ec1ce
Siddique Hameed <siddii+git@gmail.com>
2013-07-09 12:01:27 -0500

    WIP

--- extension/js/extension.js
+++ extension/js/extension.js
@@ -7,8 +7,8 @@ function GoogleAjaxFeedService($http) {
 }
 GoogleAjaxFeedService.$inject = ['$http'];
 
-function WikipediaFeeds(GoogleAjaxFeedService, extensionURL, LocalStorageService) {
-    function parseFeeds(res, baseURL) {
+function WikipediaFeeds(GoogleAjaxFeedService, $http, LocalStorageService) {
+    function parseFeeds(res) {
         var feedEntries = res.data.responseData.feed.entries;
         if (feedEntries.length > 0) {
             feedEntries.sort(function (feed1, feed2) {
@@ -18,25 +18,54 @@ function WikipediaFeeds(GoogleAjaxFeedService, extensionURL, LocalStorageService
         return feedEntries;
     }
 
-    this.loadFeeds = function (feedURL, baseURL) {
-        var feedData = LocalStorageService.getCache(feedURL);
+    this.loadFeeds = function (tab) {
+        var feedUrl = tab.feedUrl;
+        var feedData = LocalStorageService.getCache(feedUrl);
         if (feedData !== null) {
             return feedData;
         }
-        return GoogleAjaxFeedService.getFeed(feedURL).then(function(res){
-            var feedData = parseFeeds(res, baseURL);
-            LocalStorageService.setCache(feedURL, feedData);
-            return feedData;
-        });
+        if (tab.feedType === 'atom') {
+            return $http.get(feedUrl,
+                {transformResponse: function (data){
+                    console.log('data = ', data);
+                    var parser = new window.DOMParser();
+                    var xmlDoc = parser.parseFromString( data, "text/xml" );
+                    var entries = xmlDoc.getElementsByTagName('entry');
+                    var jsonResponse = [];
+                    for(var i=0; i < entries.length; i++) {
+                        console.log('entries[i] = ', entries[i]);
+                        console.log('#######    Title = ',typeof entries[i].getElementsByTagName('title')[0]);
+                        jsonResponse.push({title: entries[i].getElementsByTagName('title')[0].innerHTML,
+                            link: entries[i].getElementsByTagName('link')[0].innerText,
+                            content: entries[i].getElementsByTagName('summary')[0].innerHTML});
+                    }
+                    return jsonResponse;
+                }}).success(function (response) {
+                    console.log('response = ', response);
+                return response;
+            });
+        }
+        else {
+            return GoogleAjaxFeedService.getFeed(feedUrl).then(function(res){
+                var feedData = parseFeeds(res);
+                LocalStorageService.setCache(feedUrl, feedData);
+                return feedData;
+            });
+        }
     };
 }
-WikipediaFeeds.$inject = ['GoogleAjaxFeedService', 'extensionURL', 'LocalStorageService'];
+WikipediaFeeds.$inject = ['GoogleAjaxFeedService', '$http', 'LocalStorageService'];
 
 var selectedTabPrefKey = 'selectedTab';
 
 function WikipediaAppController($scope, $http, WikipediaFeeds, LocalStorageService, extensionURL, $templateCache) {
 
     $templateCache.put('templates/feed.html', $http.get(extensionURL + 'templates/feed.html'));
+
+//    $http.get('http://commons.wikimedia.org/w/api.php?action=featuredfeed&feed=motd&feedformat=atom&language=en').success(function (response) {
+//        console.log('response = ', response);
+//        angular.element(response).find('entry');
+//    });
 
     $scope.Feeds = {};
     $scope.FeedIndex = {};
@@ -64,7 +93,7 @@ function WikipediaAppController($scope, $http, WikipediaFeeds, LocalStorageServi
     $scope.loadTab = function (tab){
         $scope.selectedTab = tab;
         if (!$scope.Feeds[tab.id]) {
-            $scope.Feeds[tab.id] = WikipediaFeeds.loadFeeds(tab.feedUrl, $scope.tabs.baseUrl);
+            $scope.Feeds[tab.id] = WikipediaFeeds.loadFeeds(tab);
         }
     };
 }
